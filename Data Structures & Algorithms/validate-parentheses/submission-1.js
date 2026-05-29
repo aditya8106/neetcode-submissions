c65c1a37764e5d67a0e2d7ab1164ca13fff0006f
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-05-29 11:40:15 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = []
+        let map = {
+        ')':'(',
+        ']':'[',
+        '}':'{'
+        }
+        for(let char of s){
+            if(map[char]){
+                if(stack.length==0||stack[stack.length-1]!==map[char]){
+                    return false;
+                }
+                stack.pop()
+            }else{
+                stack.push(char)
+            }
+        }
+        return stack.length ===0;
+    }
+}

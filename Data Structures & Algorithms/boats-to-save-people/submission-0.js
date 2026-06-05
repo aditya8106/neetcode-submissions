27a6aa46d14141fb042cba77f0be2fc13e340b17
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-06-05 11:53:41 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        people.sort((a,b) => a-b);
+        let n = people.length
+        let  left =0
+        let right =n-1
+        let boats =0
+        while(left <= right ){
+            let sum = people[left] + people[right]
+            if(sum<=limit){
+                left++
+            }
+            right--
+            boats++
+            
+        }
+        return boats
+    }
+}

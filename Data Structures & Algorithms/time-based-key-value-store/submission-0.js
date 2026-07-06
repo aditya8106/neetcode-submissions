2ed318037a1a27437a6c9b321bb02900f2d28299
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-07-06 17:12:08 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.js
@@ -0,0 +1,48 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if(!this.keyStore.has(key)){
+            this.keyStore.set(key,[])
+        }
+        this.keyStore.get(key).push([timestamp , value])
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+
+        if(!this.keyStore.get(key)){
+            return ''
+        }
+        let arr = this.keyStore.get(key)
+        let left = 0
+        let right = arr.length - 1
+        let ans = ''
+        while(left <= right){
+            let mid = Math.floor((left+right)/2)
+            if(arr[mid][0] == timestamp){
+                ans = arr[mid][1]
+            }
+            if(arr[mid][0] <= timestamp){
+                ans = arr[mid][1]
+                left = mid +1
+            }else{
+                right = mid -1
+            }
+        }
+        return ans
+        
+    }
+}

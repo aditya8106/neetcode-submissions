b047cacbb00188111754c4a3e360eb8dc87b6028
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-07-19 23:51:05 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        let deque = []
+        let ans = []
+        for(let i = 0;i<nums.length;i++){
+            while(deque.length && deque[0] <= i - k){
+                deque.shift()
+            }
+            while(deque.length && nums[deque[deque.length - 1]] <  nums[i] ){
+                deque.pop()
+            }
+            deque.push(i)
+            if(i >=k-1){
+                ans.push(nums[deque[0]])
+            }
+        }
+        return ans
+    }
+}

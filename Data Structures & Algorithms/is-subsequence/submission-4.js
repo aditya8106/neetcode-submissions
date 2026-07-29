760225e45bacc11a29239fc8470ae97d87e23079
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-07-29 21:54:04 +0530

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+      let i = 0;
+      let j  =0;
+      while(i<=s.length && j<=t.length){
+        if(s[i] == t[j]){
+            i++
+            j++
+        }else if(s[i] !== t[j]){
+            j++
+        }
+        if(i === s.length){
+            return true
+        }
+      }
+      return false
+    }
+}

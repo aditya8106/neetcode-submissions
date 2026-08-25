906ddd7ce4467c05a9fa089a6601fe9bbd9b45d0
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-08-25 20:05:33 +0530

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} logs
+     * @return {number}
+     */
+    minOperations(logs) {
+        let depth = 0
+        for(let i = 0;i<logs.length;i++){
+            if(logs[i] == '../'){
+                if(depth > 0){
+                    depth--
+                }
+            }else if(logs[i] !=='./'){
+                depth++
+            }
+        }
+        return depth
+    }
+}

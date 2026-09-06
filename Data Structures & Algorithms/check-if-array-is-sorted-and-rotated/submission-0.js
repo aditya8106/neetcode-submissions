dd18c20f43e04059bbaeba611ffed936dc03e182
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-09-06 14:00:31 +0530

    Add: check-if-array-is-sorted-and-rotated - submission-0

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    check(nums) {
+         let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[(i + 1) % nums.length]) {
+            count++;
+        }
+    }
+
+    return count <= 1;
+    }
+}

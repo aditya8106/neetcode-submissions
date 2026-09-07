e3f6b32e4201838031ed3dcdf9e1cc8ed6a0c4d4
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-09-07 13:35:10 +0530

    Add: divide-array-into-equal-pairs - submission-0

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    divideArray(nums) {
+        const freq = new Map();
+
+        for (const num of nums) {
+            freq.set(num, (freq.get(num) || 0) + 1);
+        }
+        for (const count of freq.values()) {
+            if (count % 2 !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

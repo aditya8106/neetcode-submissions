71efa91347b391b98362008f654bb0ee61c79f12
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-09-18 23:03:52 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} ransomNote
+     * @param {string} magazine
+     * @return {boolean}
+     */
+    canConstruct(ransomNote, magazine) {
+        let map = new Map();
+
+        for (let char of magazine) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        for (let char of ransomNote) {
+            if (!map.has(char) || map.get(char) === 0) {
+                return false;
+            }
+            map.set(char, map.get(char) - 1);
+        }
+        return true;
+
+    }
+}

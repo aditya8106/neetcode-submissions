940814c359c135c8a48b696e585098a2afcc62f9
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-09-25 21:58:35 +0530

    Add: sort-array-by-parity - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArrayByParity(nums) {
+       let left = 0
+       let right = nums.length -1
+       while(left <= right){
+        if(nums[left] % 2 !==0 && nums[right] % 2 == 0){
+            [nums[left] , nums[right]] = [nums[right] , nums[left]]
+            left++
+            right--
+        }else {
+                if (nums[left] % 2 === 0) left++;
+                if (nums[right] % 2 !== 0) right--;
+            }
+       }
+       return nums
+    }
+}

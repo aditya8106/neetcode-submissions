2b1f34a2ef713dea4aecd74707158aff0bc62484
Y .ADITYA MARUTHI NANDAN <adithyayegiti93@gmail.com>
2026-09-26 22:12:43 +0530

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestMonotonicSubarray(nums) {
+        if (nums.length === 0) return 0;
+
+        let maxLen = 1;
+        let inc = 1;
+        let dec = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                inc++;
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                dec++;
+                inc = 1;
+            } else {
+                inc = 1;
+                dec = 1;
+            }
+
+            maxLen = Math.max(maxLen, inc, dec);
+        }
+
+        return maxLen;
+    }
+}
